da2c0f11d1c876ee06787c27a1127acb813c9665
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2023-09-27 19:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
-
 function palindrome(str){
+	let arr ='';
+	for(let i = str.length-1; i>=0 ; i--){
+		let char = str.charAt(i);
+		if(/[a-zA-Z]/.test(char)){
+			arr += char;
+		}
+	}
+	return arr;
 
 }
+
+
 module.exports = palindrome
